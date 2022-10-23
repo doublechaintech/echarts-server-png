08c2caef0257c79837eef0385772d7f6cb56eeba
Philip Zhang <zhangxilai@doublechaintech.com>
2022-10-23 15:04:19 +0800

    🚀add error image

--- src/error-image.js
+++ src/error-image.js
@@ -19,7 +19,16 @@ const ErrorImage=({req,res,text})=>{
 
     ctx.font = '30px serif';
     ctx.textAlign = 'center';
-    ctx.fillText(text, x, 300);
+    ctx.fillText(text, x, 280);
+    ctx.beginPath();
+    ctx.moveTo(0, 500);
+    ctx.lineTo(800, 500);
+    ctx.stroke();
+
+    ctx.textAlign = 'left';
+    ctx.font = '10px serif';
+    ctx.fillText("成都双链科技 图片服务@2022", 20, 550);
+
     res.writeHead(200, {'Content-Type': 'image/png'});
     res.write(canvas.toBuffer('image/png'));
     res.end();
